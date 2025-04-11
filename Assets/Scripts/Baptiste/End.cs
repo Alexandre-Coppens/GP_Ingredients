dd58401a40c77ae346ff0f01cfc13e7d26d13581
Baptiste-Verdier <baptiste.verdier@artfx.fr>
2025-04-11 14:43:39 +0200

    [Add] Level building + end level

---
+++ Assets/Scripts/Baptiste/End.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) { SceneManager.LoadScene("EndGame"); }
+    }
+}
